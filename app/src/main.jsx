2b89cbb88927639709f22c6d38cfd622ba8b7b61
Rohit <rrgawanderohit@gmail.com>
2025-04-11 17:08:05 +0530

    changed dependencies

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import ShoppingCartProvider from "./context/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Final-React-Shopping-Cart">
     <ShoppingCartProvider>
       <App />
     </ShoppingCartProvider>
